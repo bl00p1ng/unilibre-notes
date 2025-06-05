dd3495a904a406d0ba947f9fe840495a67b22489
bl00p1ng <blooping@protonmail.com>
2025-06-05 15:53:31 -0500

    chore: update convex dependency version to ^1.8.0 in package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "files.edgestore.dev",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "files.edgestore.dev",
+            },
+        ],
+    },
+    experimental: {
+        esmExternals: true,
+    },
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
