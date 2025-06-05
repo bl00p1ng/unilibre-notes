aecb3c6a133f64ca7db8cc82e849280fb4ec7b23
bl00p1ng <blooping@protonmail.com>
2025-06-05 16:14:12 -0500

    feat: localize upload instruction in SingleImageDropzone component to Spanish

--- components/single-image-dropzone.tsx
+++ components/single-image-dropzone.tsx
@@ -152,7 +152,7 @@ const SingleImageDropzone = React.forwardRef<HTMLInputElement, InputProps>(
             <div className="flex flex-col items-center justify-center text-xs text-gray-400">
               <UploadCloudIcon className="mb-2 h-7 w-7" />
               <div className="text-gray-400">
-                Click or drag to this area to upload
+                Haz click o arrastra un archivo aquí
               </div>
             </div>
           )}
