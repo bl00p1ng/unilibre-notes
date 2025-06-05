35b934f947d3183d7fc16a76f088d887381b9aa6
bl00p1ng <blooping@protonmail.com>
2025-06-05 16:04:34 -0500

    feat: localize document creation and navigation components to Spanish

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -10,48 +10,48 @@ import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 
 const DocumentsPage = () => {
-  const { user } = useUser();
-  const router = useRouter();
-  const create = useMutation(api.documents.create);
+    const { user } = useUser();
+    const router = useRouter();
+    const create = useMutation(api.documents.create);
 
-  const onCreate = () => {
-    const promise = create({ title: "Untitled" }).then((documentId) =>
-      router.push(`/documents/${documentId}`),
+    const onCreate = () => {
+        const promise = create({ title: "Sin título" }).then((documentId) =>
+            router.push(`/documents/${documentId}`),
+        );
+
+        toast.promise(promise, {
+            loading: "Creando una nueva asinatura...",
+            success: "Nueva asignatura creada!",
+            error: "Error al crear una nueva asignatura.",
+        });
+    };
+
+    return (
+        <div className="flex h-full flex-col items-center justify-center space-y-4">
+            <Image
+                src="/empty.svg"
+                alt="empty"
+                height="300"
+                width="300"
+                priority
+                className="h-auto dark:hidden"
+            />
+            <Image
+                src="/empty-dark.svg"
+                alt="empty"
+                height="300"
+                width="300"
+                priority
+                className="hidden h-auto dark:block"
+            />
+            <h2 className="text-lg font-medium">
+                Bienvenido a Unilibre Notes {user?.firstName}
+            </h2>
+            <Button onClick={onCreate}>
+                <PlusCircle className="mr-2 h-4 w-4" />
+                Crear una asignatura
+            </Button>
+        </div>
     );
-
-    toast.promise(promise, {
-      loading: "Creating a new note....",
-      success: "New note created!",
-      error: "Failed to create a new note.",
-    });
-  };
-
-  return (
-    <div className="flex h-full flex-col items-center justify-center space-y-4">
-      <Image
-        src="/empty.svg"
-        alt="empty"
-        height="300"
-        width="300"
-        priority
-        className="h-auto dark:hidden"
-      />
-      <Image
-        src="/empty-dark.svg"
-        alt="empty"
-        height="300"
-        width="300"
-        priority
-        className="hidden h-auto dark:block"
-      />
-      <h2 className="text-lg font-medium">
-        Welcome to {user?.firstName}&apos;s Zotion
-      </h2>
-      <Button onClick={onCreate}>
-        <PlusCircle className="mr-2 h-4 w-4" />
-        Create a note
-      </Button>
-    </div>
-  );
 };
 export default DocumentsPage;
