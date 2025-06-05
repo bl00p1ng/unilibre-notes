d121cd1c33cb66f268061962694fa750417cc3d8
bl00p1ng <blooping@protonmail.com>
2025-06-05 15:43:17 -0500

    Remove generated version information from auto-generated files and update auth provider domain

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://sweet-stinkbug-98.clerk.accounts.dev",
+      domain: "https://daring-grouse-92.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
